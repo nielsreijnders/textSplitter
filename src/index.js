b2211f6b6a06de1a5899d7df1f6f97570b7c8c0a
Niels <niels.reijnders@edu-kw1c.com>
2019-01-31 11:50:53 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+export function splitLetters(container, opentag, closingtag) {
+    container.innerHTML = container.textContent.replace(/\S/g, opentag + "$&" + closingtag);
+}
+
+export function splitWords(container, opentag, closingtag) {
+    container.innerHTML = container.textContent.replace(/\S+/g, opentag + "$&" + closingtag);
+}
+
+export function splitLines(container, opentag, closingtag) {  
+    var spans = container.children,
+        top = 0,
+        tmp = opentag;  
+    container.innerHTML = container.textContent.replace(/\S+/g, '<n>$&</n>');     
+    for (let i = 0; i < spans.length; i++) {
+        var rect = spans[i].getBoundingClientRect().top;
+        if (top < rect) {
+            tmp += closingtag + opentag;
+            top = rect;
+        } 
+        tmp += spans[i].textContent + ' ';   
+    } 
+    container.innerHTML = tmp += closingtag;
+}
+
+export default {splitLetters, splitLines, splitWords}
