14671fc879250730f2886d6ffe7ff0600d5a5ca8
Niels <niels.reijnders@edu-kw1c.nlm>
2019-06-28 08:48:26 +0200

    changed regular expression

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 export function splitLetters(container, opentag, closingtag) {
-    container.innerHTML = container.textContent.replace(/\S/g, opentag + "$&" + closingtag);
+    container.innerHTML = container.textContent.replace(/./g, opentag + "$&" + closingtag);
 }
 
 export function splitWords(container, opentag, closingtag) {
-    container.innerHTML = container.textContent.replace(/\S+/g, opentag + "$&" + closingtag);
+    container.innerHTML = container.textContent.replace(/./g, opentag + "$&" + closingtag);
 }
 
 export function splitLines(container, opentag, closingtag) {
     var spans = container.children,
         top = 0,
         tmp = '';
-    container.innerHTML = container.textContent.replace(/\S+/g, '<n>$&</n>');
+    container.innerHTML = container.textContent.replace(/./g, '<n>$&</n>');
     for (let i = 0; i < spans.length; i++) {
         var rect = spans[i].getBoundingClientRect().top;
         if (top < rect) tmp += closingtag + opentag;
