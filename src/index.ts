0580ec485e2ecddebd5fcbda656e54f9b8211f1d
nielsreijnders <niels.reijnders@edu-kw1c.nl>
2022-08-25 11:24:14 +0200

    feat(splitlines): add html tags support

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,23 @@ export function splitWords(container: HTMLElement, opentag: string, closingtag: 
 }
 
 export function splitLines(container: HTMLElement, opentag: string, closingtag: string) {
-    var spans = container.children,
+    splitWords(container, '<n>', '</n>');
+
+    var spans = container.querySelectorAll('n'),
+        htmlTags = container.children,
+        whiteSpace = container.innerHTML,
         top = 0,
         tmp = '';
 
-    container.innerHTML = container.innerHTML.replace(/\S+/g, '<n>$&</n>');
     for (let i = 0; i < spans.length; i++) {
         var rect = Math.abs(spans[i].getBoundingClientRect().top);
         if (top < rect || top > rect) tmp += closingtag + opentag;
         top = rect;
-        tmp += spans[i].innerHTML + ' ';
+
+        const replacedHtmlTags = htmlTags[i].outerHTML.toString();
+
+        // Maybe better to use regex to replace the html tags since this might be a performance issue
+        tmp += replacedHtmlTags.replace('<n>', '').replace('</n>', '') + (whiteSpace.includes(replacedHtmlTags + ' ') ? ' ' : '');
     }
 
     container.innerHTML = tmp += closingtag;
