9a39255f1586c62f12b5fe69f80e9785577b6f43
Niels Reijnders <36259700+nielsreijnders@users.noreply.github.com>
2023-01-04 11:07:04 +0100

    feat(splitlines): preverse html tags

--- README.md
+++ README.md
@@ -1,51 +1,56 @@
-# TextSplitter 🥭
+# TextSplitter 🔪
 
 [![npm version](https://img.shields.io/npm/v/textsplitter.svg?style=for-the-badge&colorB=ACC7C3)](https://www.npmjs.com/package/textsplitter)
 [![gzip size](https://img.shields.io/bundlephobia/minzip/textsplitter.svg?colorB=ACC7C3&label=GZIP%20SIZE&style=for-the-badge)](https://unpkg.com/textsplitter)
 [![license](https://img.shields.io/npm/l/textsplitter.svg?style=for-the-badge&colorB=ACC7C3)](https://github.com/nielsreijnders/textsplitter/blob/master/LICENSE)
 [![dependencies](https://img.shields.io/badge/dependencies-none-ff69b4.svg?style=for-the-badge&colorB=ACC7C3)](https://github.com)
 
-Tiny library to split each characters, words or lines! 
+Tiny library to split each characters, words or lines! New feature for splitlines function to preserve HTML tags.
 
-##### Install
+#### Install
 
 ```js
 npm install textsplitter --save
 ```
 
-##### Usage
+#### Usage
 
 ```js
-import {splitLetters, splitWords, splitLines} from 'textsplitter';
+import { splitLetters, splitWords, splitLines } from 'textsplitter';
 
-//Openingtag & closingtag has to be a string
+// HTML ELEMENT, STRING, STRING
 splitLetters(container, openingtag, closingtag);
 
-//It is pretty simple so..
+// HTML ELEMENT, STRING, STRING
 splitWords(container, openingtag, closingtag);
 
-//So I don't really need a documentation right?
+// HTML ELEMENT, STRING, STRING
 splitLines(container, openingtag, closingtag);
 
 // React usage
 useEffect(() => {
-  // It is not specific made for React but obv it works fine
+  // It is not specific made for React but it works fine
   splitLines(ref.current, openingtag, closingtag);
-}, [])
 
+  // If not calulating correctly this might be an issue with the fonts which are not ready yet 
+  document.fonts.ready.then(() => {
+    splitLines(ref.current, openingtag, closingtag);
+  });
+
+  // When running React in strict mode you might want to add an if statement to check if the function already fired
+  if (!ref.current.querySelector('.lines')) {
+    splitLines(ref.current, '<span class="lines">', '</span>');
+  }
+}, [])
 ```
 
-##### Example
+#### Example
 
 ```js
 splitLines(document.getElementById("container"), "<span><thisiscoolhuh>", "</thisiscoolhuh></span>");
 ```
-###### output
+#### Output
 
 ![example.gif](https://media.giphy.com/media/jxchZz0EDhQ7QmYEwD/giphy.gif)
 
-##### Notice
-
-> This library only breaks each line / word or letter! You can add your own animations with by example css or gsap.
-
-###### MIT © <a href="#?????portfolio_coming_soon">Niels Reijnders</a>
+#### MIT © <a href="nielsreijnders.nl">Niels Reijnders</a>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/prefer-optional-chain */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
@@ -28,44 +29,85 @@ export function splitWords (container: HTMLElement, opentag: string, closingtag:
   return container
 }
 
-export function splitLines (container: HTMLElement, opentag: string, closingtag: string): HTMLElement {
+export function splitLines (container: HTMLElement, opentag: string, closingtag: string, customClass?: string): HTMLElement {
   splitWords(container, '<n>', '</n>')
 
   let lastY = -9999
-  const array: any = []
+
   container.querySelectorAll('n').forEach((node) => {
     const { y } = node.getBoundingClientRect()
 
     if (y > lastY) {
       lastY = y
-      array.push(`${closingtag}${opentag}${node.innerHTML}`)
+      node.outerHTML = `<line>${node.innerHTML}</line>`
+
+      return
+    }
+
+    node.outerHTML = `${node.innerHTML}`
+  })
+
+  let output = ''
+  let index = 0
+
+  // Array for storing opening tags for the next line
+  const nextLineOpeningTags: any = [];
+
+  // Loop through each line
+  // Because it is possible that the FIRST line tag is not at the beginning of the string it might not get the correct opening tags
+  // To solve this issue remove the first line tag and add it at the beginning of the string
+  (`<line>${container.innerHTML.trim().replace('<line>', '')}`).split('<line>').forEach((str) => {
+    if (!str) return
+
+    // Remove closing tag from the end of the line
+    const string = str.replace('</line>', '')
+
+    // Get opening tags from the current line
+    // ALSO SKIP ALL BR TAGS
+    const openingtags: string[] = string.match(/<(?!\/)(?!br)[^>]+>/g)! ?? []
+
+    // Get closing tags from the current line
+    const closingtags: string[] = string.match(/<\/[^>]+>/g)! ?? []
+
+    // Get opening tags from the previous line if not exist return an empty array
+    const openingTagsLastLine = nextLineOpeningTags[index - 1] ?? []
+
+    // Filter opening tags that are not closed
+    const filteredOpeningTagsEndOfLine = [...openingtags, ...openingTagsLastLine].slice().reverse().filter((_tag, index) => {
+      if (!closingtags[index]) return true
+      return false
+    })
+
+    // Get all filtered opening tags and replpace them to closing tags
+    const filteredClosingTagsEndOfline = filteredOpeningTagsEndOfLine.map(tag => tag.replace('<', '</')).join('')
+
+    // Set as string if possible
+    const openingTagsLastLineToString = openingTagsLastLine ? openingTagsLastLine.slice().reverse().join('') : ''
+
+    // Filter opening tags from the previous line that are not closed
+    const filteredClosingTagsIfOpeningTagIsPresent = openingTagsLastLine && openingTagsLastLine.filter((_tag: any, index: number) => {
+      if (!closingtags[index]) return true
+      return false
+    }).map((tag: any) => tag.replace('<', '</'))
+
+    // Set as string if possible
+    const filteredClosingTagsIfOpeningTagIsPresentToString = filteredClosingTagsIfOpeningTagIsPresent ? filteredClosingTagsIfOpeningTagIsPresent.join('') : ''
+
+    // If there is a openingtag present without a closing tag push the opening tag from last line
+    if (filteredClosingTagsIfOpeningTagIsPresentToString) {
+      nextLineOpeningTags.push(openingTagsLastLine.splice(-filteredClosingTagsIfOpeningTagIsPresent.length))
     } else {
-      array.push(node.innerHTML)
-    }
-  })
-
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  container.innerHTML = `${array.join(' ')}${closingtag}`
-
-  return container
-}
-
-export function richtextSplitLines (container: HTMLElement, opentag: string, closingtag: string, customClass?: string): HTMLElement {
-  splitWords(container, `${opentag}<n>`, `</n>${closingtag}`)
-
-  let lastY = -9999
-  let count = 0
-
-  container.querySelectorAll('n').forEach((node) => {
-    const { y } = node.getBoundingClientRect()
-
-    if (y > lastY) {
-      count += 1
-      lastY = y
+      // Otherwise push opening tags from the end line which will be used in the next line
+      nextLineOpeningTags.push(filteredOpeningTagsEndOfLine)
     }
 
-    node.outerHTML = `<span class="${customClass ?? ''}line-${count}">${node.innerHTML}</span>`
+    const refactoredLine = `${opentag}${openingTagsLastLineToString}${string}${filteredClosingTagsEndOfline}${filteredClosingTagsIfOpeningTagIsPresentToString}${closingtag}`
+
+    output += refactoredLine
+    index += 1
   })
+
+  container.innerHTML = output
 
   return container
 }
