aaae54109662d975c65768e1f5f4c62316af356c
Niels Reijnders <36259700+nielsreijnders@users.noreply.github.com>
2023-01-04 15:17:43 +0100

    fix(splitlines): filter issue

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,8 @@ export function splitLines (container: HTMLElement, opentag: string, closingtag:
   let index = 0
 
   // Array for storing opening tags for the next line
-  const nextLineOpeningTags: any = [];
+  const nextLineOpeningTags: any = []
+  const nextLineClosingTags: any = [];
 
   // Loop through each line
   // Because it is possible that the FIRST line tag is not at the beginning of the string it might not get the correct opening tags
@@ -72,41 +73,25 @@ export function splitLines (container: HTMLElement, opentag: string, closingtag:
     // Get opening tags from the previous line if not exist return an empty array
     const openingTagsLastLine = nextLineOpeningTags[index - 1] ?? []
 
+    // Get opening tags from the previous line if not exist return an empty array
+    const closingTagsLastLine = nextLineClosingTags[index - 1] ?? []
+
+    openingtags.push(...openingTagsLastLine)
+    closingtags.push(...closingTagsLastLine)
+
     // Filter opening tags that are not closed
-    const filteredOpeningTagsEndOfLine = [...openingtags, ...openingTagsLastLine].slice().reverse().filter((_tag, index) => {
-      if (!closingtags[index]) return true
-      return false
-    })
+    const filteredOpeningTagsEndOfLine = openingtags.slice().splice(0, openingtags.length - closingtags.length)
 
-    // Get all filtered opening tags and replpace them to closing tags
-    const filteredClosingTagsEndOfline = filteredOpeningTagsEndOfLine.map(tag => tag.replace('<', '</')).join('')
+    nextLineOpeningTags.push(filteredOpeningTagsEndOfLine)
+    const refactoredLine = `${opentag}${openingTagsLastLine.join('')}${string}${filteredOpeningTagsEndOfLine.slice().reverse().map(tag => tag.replace('<', '</')).join('')}${closingtag}`
 
-    // Set as string if possible
-    const openingTagsLastLineToString = openingTagsLastLine ? openingTagsLastLine.slice().reverse().join('') : ''
-
-    // Filter opening tags from the previous line that are not closed
-    const filteredClosingTagsIfOpeningTagIsPresent = openingTagsLastLine && openingTagsLastLine.filter((_tag: any, index: number) => {
-      if (!closingtags[index]) return true
-      return false
-    }).map((tag: any) => tag.replace('<', '</'))
-
-    // Set as string if possible
-    const filteredClosingTagsIfOpeningTagIsPresentToString = filteredClosingTagsIfOpeningTagIsPresent ? filteredClosingTagsIfOpeningTagIsPresent.join('') : ''
-
-    // If there is a openingtag present without a closing tag push the opening tag from last line
-    if (filteredClosingTagsIfOpeningTagIsPresentToString) {
-      nextLineOpeningTags.push(openingTagsLastLine.splice(-filteredClosingTagsIfOpeningTagIsPresent.length))
-    } else {
-      // Otherwise push opening tags from the end line which will be used in the next line
-      nextLineOpeningTags.push(filteredOpeningTagsEndOfLine)
-    }
-
-    const refactoredLine = `${opentag}${openingTagsLastLineToString}${string}${filteredClosingTagsEndOfline}${filteredClosingTagsIfOpeningTagIsPresentToString}${closingtag}`
+    console.log(refactoredLine)
 
     output += refactoredLine
     index += 1
   })
 
+  console.log(output)
   container.innerHTML = output
 
   return container
